b4745bc303908aa810295396e7e4a9703115894c
Arak <pablo.arak.vm@gmail.com>
2023-10-13 14:30:27 -0300

    Added basic services for products

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -11,8 +11,12 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 
+import { ProductsService } from 'src/services/products.service';
+
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   @Get()
   getProducts(@Query('limit') limit = 100, @Query('offset') offset = 0) {
     return {
@@ -30,31 +34,21 @@ export class ProductsController {
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId') productId: string) {
-    return {
-      message: '',
-    };
+    return this.productsService.findOne(+productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      message: 'accion de actualizar',
-      payload,
-    };
+    return this.productsService.update(id, payload);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return {
-      message: 'accion de eliminar',
-    };
+  delete(@Param('id') id: number) {
+    return this.productsService.delete(id);
   }
 }
