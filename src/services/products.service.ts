fe11d082927e430abe4d6867625a972f1efd9bdb
Arak <pablo.arak.vm@gmail.com>
2023-10-13 15:02:20 -0300

    Added throw custom exception in findOne product

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './../entities/product.entity';
 
 @Injectable()
@@ -20,7 +20,11 @@ export class ProductsService {
   }
 
   findOne(id: number) {
-    return this.products.find((item) => item.id === id);
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
   }
 
   create(payload: any) {
