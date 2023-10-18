7d415bfb7989893e8022f0a34c7afe36085af262
Arak <pablo.arak.vm@gmail.com>
2023-10-18 10:00:35 -0300

    Added more env config (include typo error control)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+import { environments } from './environments';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
     }),
     UsersModule,
